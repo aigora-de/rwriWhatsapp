c2093c8a77bddcd4e92d8c06326f242b202d7bd7
aigora-de <23141680+aigora-de@users.noreply.github.com>
2021-01-03 21:59:03 +0000

    Basic class to parse _chat.txt export and load into a pandas dataframe.

--- rwriWhatsapp/rwriWhatsapp.py
+++ rwriWhatsapp/rwriWhatsapp.py
@@ -1,10 +1,21 @@
-import re
-import networkx as nx
+
 import pandas as pd
+from whatsapp_chat_parser import get_messages
 
 
 class WhatsAppParser:
 
     @staticmethod
     def run():
-        print("Hello World...")
+        messages = get_messages("../data/_chat.txt")
+        for message in messages["chats"]:
+            print(message["author"])
+            print(message["message"])
+            print(message["timestamp"])
+
+        for descriptive_message in messages["descriptive_messages"]:
+            print(descriptive_message)
+
+        df = pd.DataFrame(messages['chats'])
+        print(df.head())
+
